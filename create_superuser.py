57bac8d0453bf361aad9f7b5fd69b8a72b551a88
GabrielRehbein <gabrielrehbei@gmail.com>
2025-01-23 08:04:26 -0800

    application dockered

---
+++ create_superuser.py
@@ -0,0 +1,24 @@
+import os
+from decouple import config
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+
+import django
+django.setup()
+
+
+from django.contrib.auth.models import User
+
+
+DJANGO_ADM_USERNAME = config('DJANGO_ADM_USERNAME')
+DJANGO_ADM_EMAIL = config('DJANGO_ADM_EMAIL')
+DJANGO_ADM_PASSWORD = config('DJANGO_ADM_PASSWORD')
+
+
+if not User.objects.filter(username=DJANGO_ADM_USERNAME).exists():
+    user = User.objects.create_superuser(DJANGO_ADM_USERNAME, DJANGO_ADM_EMAIL, DJANGO_ADM_PASSWORD)
+    print(f'Superuser {user.username} created with success.')
+else:
+    print('Superuser already exist.')
