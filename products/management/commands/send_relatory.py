0fe533ffff6b434801da18c0f9410663193f3570
GabrielRehbein <gabrielrehbei@gmail.com>
2025-03-26 17:19:27 -0300

    create feature: send message every day about outflow quantity per day

--- products/management/commands/send_relatory.py
+++ products/management/commands/send_relatory.py
@@ -1,9 +1,25 @@
 from django.core.management.base import BaseCommand
-from services.notify import Notify
+from outflows.models import Outflow
+from django.utils import timezone
+from datetime import timedelta
+from services.callmebot import CallMeBotService
 class Command(BaseCommand):
     help = 'send relatory for whatsapp'
 
     def handle(self, *args, **options):
-        self.stdout.write(
-            self.style.SUCCESS('READY')
-        )
+        call_me_bot_client = CallMeBotService()
+        now = timezone.now()
+
+        today_start = now.replace(hour=0, minute=0, second=0, microsecond=0)
+        today_end = now.replace(hour=23, minute=59, second=59, microsecond=999999)
+
+        outflows_today = Outflow.objects.filter(
+            created_at__range=[today_start, today_end]
+        ).count()
+        message = {
+            'text': f'Hoje tivemos {outflows_today} vendas hoje!'
+        }
+        call_me_bot_client.send_message(message['text'])
+
+
+
